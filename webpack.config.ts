816df29cedb2ab54902b98d3e1c0b24dccf35923
Uzhastin-Nikita <fosfor20@mail.ru>
2023-11-25 13:00:48 +0300

    feat(webpack): add config to project

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+    hot: true,
+  },
+};
