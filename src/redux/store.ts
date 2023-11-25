b33812f0aaf7a807ae76938edfcbc5d27ab1ae45
Uzhastin-Nikita <fosfor20@mail.ru>
2023-11-25 15:01:38 +0300

    refactor(store): add separate type for RootState

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,9 @@
-import { createStore } from "redux";
-import reducer from "./reducer";
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+export type RootState = {
+  count: number;
+}
 
 const store = createStore(reducer);
 
