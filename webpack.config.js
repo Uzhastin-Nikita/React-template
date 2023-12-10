90b2ca1c20862a3a6e9614574c77fc52b9426b09
Uzhastin-Nikita <fosfor20@mail.ru>
2023-12-10 23:50:03 +0300

    refactor(webpack): delete comments

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "public"), // указываем папку public как корень сервера
+      directory: path.join(__dirname, "public"),
     },
     port: 3000,
     hot: true,
